2df2907f08da4f98b7b828fd6267efa81250b0bc
massa1912 <martinmassa19@gmail.com>
2023-07-12 09:33:23 -0300

    Configuracion para test

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,12 +1,22 @@
-
+import PropTypes from 'prop-types'
     
-    export const GifItem = ({ title, url, id }) => {
+export const GifItem = ({ title, url, id }) => {
 
-      return (
-        <div className="card">
-            <img src={ url } alt={ title } />
-            <p>{ title }</p>
-        </div>
-      )
-    }
+  return (
+    <div className="card">
+        <img src={ url } alt={ title } />
+        <p>{ title }</p>
+    </div>
+  )
+}
+
+GifItem.propTypes = {
+  title: PropTypes.string,
+  url: PropTypes.string.isRequired,
+}
+
+GifItem.defaultProps = {
+  title: 'No hay titulo',
+  url: 'No hay url',
+}
     
